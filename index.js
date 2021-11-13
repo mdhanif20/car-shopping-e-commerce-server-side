5b510f45017a31fc6a32a0b1fe8d14aec703a51f
mdhainf20 <mdhanif115825@gmail.com>
2021-11-13 11:20:19 +0600

    added orders

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
-// const axios = require('axios').default;
+const { query } = require("express");
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -17,10 +17,25 @@ async function run(){
         await client.connect();
         const database = client.db("carProducts");
         const products = database.collection("products");
+        const allOrders = database.collection("orders");
         app.get("/products",async(req,res)=>{
             const product = products.find({});
             const result = await product.toArray();
             res.send(result);
+        })
+        // get single product 
+        app.get("/products/:id", async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id:ObjectId(id)}
+          const order = await products.findOne(query); 
+          res.send(order);
+        })
+        // orders post 
+        app.post("/order",async(req,res)=>{
+          const orders = req.body;
+          console.log(orders);
+          const result =  await allOrders.insertOne(orders);
+          res.json(result);
         })
     }
     finally{
@@ -30,7 +45,7 @@ async function run(){
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Online car shopping!')
 })
 
 app.listen(port, () => {
